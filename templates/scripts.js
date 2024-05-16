c5520a6848cdadb087f5aabb24dc06ed1daee20c
TheOldThormas <1tomilopez@gmasl.com>
2024-05-16 14:17:03 +0000

    etetica y funcionalidad basica para front

--- templates/scripts.js
+++ templates/scripts.js
@@ -1,72 +1,78 @@
 class SvgLinkEffect {
-    constructor(effect) {
+  constructor(effect) {
       if (effect.config.random) {
-        this.randomizeArray(effect.nodes);
+          this.randomizeArray(effect.nodes);
       }
-  
+
       effect.element.addEventListener("click", () => {
-        const reverse = effect.element.classList.contains("active");
-        effect.element.classList.toggle("active");
-        effect.handler(effect.nodes, effect.config, reverse);
+          const reverse = effect.element.classList.contains("active");
+          effect.element.classList.toggle("active");
+          effect.handler(effect.nodes, effect.config, reverse);
       });
-    }
-  
-    randomizeArray(array) {
-      for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-      }
-    }
   }
-  
-  const svgBounceEffect = (nodes, config, reverse) => {
-    nodes.forEach((node, index) => {
+
+  randomizeArray(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [array[i], array[j]] = [array[j], array[i]];
+      }
+  }
+}
+
+const svgBounceEffect = (nodes, config, reverse) => {
+  nodes.forEach((node, index) => {
       const { duration, ease, y1, y2, offset } = config;
       setTimeout(() => {
-        const yValue = reverse ? y1 : y2;
-        gsap.to(node, {
-          duration: duration,
-          ease: ease,
-          y: yValue
-        });
+          const yValue = reverse ? y1 : y2;
+          gsap.to(node, {
+              duration: duration,
+              ease: ease,
+              y: yValue
+          });
       }, index * offset);
-    });
-  };
-  
-  const svgAlternatingEffect = (nodes, config, reverse) => {
-    nodes.forEach((node, index) => {
+  });
+};
+
+const svgAlternatingEffect = (nodes, config, reverse) => {
+  nodes.forEach((node, index) => {
       const { duration, ease, offset } = config;
       setTimeout(() => {
-        const yValue = reverse
-          ? (index % 2 === 0 ? -44 : 44)
-          : (index % 2 === 0 ? 44 : -44);
-        gsap.to(node, {
-          duration: duration,
-          ease: ease,
-          y: yValue
-        });
+          const yValue = reverse
+              ? (index % 2 === 0 ? -44 : 44)
+              : (index % 2 === 0 ? 44 : -44);
+          gsap.to(node, {
+              duration: duration,
+              ease: ease,
+              y: yValue
+          });
       }, index * offset);
-    });
-  };
-  
-  document.addEventListener("DOMContentLoaded", () => {
-    const cascadeLink = document.getElementById("cascade-link");
-    if (cascadeLink) {
-      const cascadeLinkEffect = {
-        element: cascadeLink,
-        handler: svgBounceEffect,
-        nodes: [...cascadeLink.querySelectorAll("rect")],
-        config: {
-          offset: 10,
-          duration: 1,
-          random: true,
-          ease: "expo.out",
-          y1: -20,
-          y2: 44
-        }
-      };
-  
-      new SvgLinkEffect(cascadeLinkEffect);
-    }
   });
-  
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const applyEffect = (id) => {
+      const element = document.getElementById(id);
+      if (element) {
+          const effect = {
+              element: element,
+              handler: svgBounceEffect,
+              nodes: [...element.querySelectorAll("rect")],
+              config: {
+                  offset: 10,
+                  duration: 1,
+                  random: true,
+                  ease: "expo.out",
+                  y1: -20,
+                  y2: 44
+              }
+          };
+
+          new SvgLinkEffect(effect);
+      }
+  };
+
+  applyEffect("cascade-link");
+  applyEffect("button-2");
+  applyEffect("button-3");
+  applyEffect("button-4");
+});
